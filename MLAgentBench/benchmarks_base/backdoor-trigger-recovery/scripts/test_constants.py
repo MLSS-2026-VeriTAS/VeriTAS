e431c1058500b7250bbfe8ca276acf5a6dfa2fe2
Zach-Attach <zyle@instance-20260622-024946.c.veritas-500122.internal>
2026-06-22 03:46:27 +0000

    Revert "copying MLRC (#12)"
    This reverts commit d7541e4b3950bb429fd36870af420528eb3e08a0.

--- MLAgentBench/benchmarks_base/backdoor-trigger-recovery/scripts/test_constants.py
+++
@@ -1,4 +0,0 @@
-test_model_id = "Zhaorun/CodeQwen1.5-7B-trojan-clas2024-development"
-test_trigger_data = "trigger_gt.json"
-test_target_list = "target_list.json"
-test_prompt_data = "test.jsonl" # change to "test.jsonl" for public test
--- MLAgentBench/benchmarks_base/product-recommendation/scripts/test_constants.py
+++
@@ -1,4 +0,0 @@
-train_data_path = 'data/train.csv'
-test_data_path = 'data/test_features.csv'
-labels_path = 'data/test_labels.csv'
-PREDS_PER_SESSION = 100
